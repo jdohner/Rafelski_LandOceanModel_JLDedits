fdeb58e92aa26456465855615e86bcbbde48fbf5
Julia Dohner <jdohner@cs.hmc.edu>
2017-09-18 12:10:27 -0700

    added defaults file for land model folder

---
+++ co2_model_code_JLD/defaults.m
@@ -0,0 +1,10 @@
+% file defaults.m
+%
+% author Julia Dohner
+%
+% brief Includes code that when run, will add paths for code to access 
+% other folders. Run defaults.m before running the land model code.
+
+addpath(genpath('/Users/juliadohner/Documents/MATLAB/Dohner edits of Rafelski Code/co2_decon_code_JLD'));
+
+
