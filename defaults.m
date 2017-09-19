17917b9528504eb8b658fcb0a56973c511884e6e
Julia Dohner <jdohner@cs.hmc.edu>
2017-09-19 06:18:00 -0700

    changed around some folder names, separated data into own folder

---
+++ defaults.m
@@ -0,0 +1,18 @@
+% file defaults.m
+%
+% author Julia Dohner
+%
+% brief Includes code that when run, will add paths for code to access 
+% other folders. Run defaults.m before running the land model code.
+
+%co2_decon_code_JLD
+addpath(genpath('/Users/juliadohner/Documents/MATLAB/Dohner edits of Rafelski Code/co2_decon_code_JLD'));
+
+%co2_model_code_JLD
+addpath(genpath('/Users/juliadohner/Documents/MATLAB/Dohner edits of Rafelski Code/co2_model_code_JLD'));
+
+%co2_model_data
+addpath(genpath('/Users/juliadohner/Documents/MATLAB/Dohner edits of Rafelski Code/co2_model_data'));
+
+
+
