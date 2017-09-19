43af4edd2b2bf82e153a62f3dbb912af34e8adcd
Julia Dohner <jdohner@cs.hmc.edu>
2017-09-19 11:10:54 -0700

    Moved things around and added the missing file for airborne fraction folder forward_run_fit.m

--- defaults.m
+++ defaults.m
@@ -3,16 +3,16 @@
 % author Julia Dohner
 %
 % brief Includes code that when run, will add paths for code to access 
-% other folders. Run defaults.m before running the land model code.
+% other folders. Run defaults.m before running any model code.
 
 %co2_decon_code_JLD
-addpath(genpath('/Users/juliadohner/Documents/MATLAB/Dohner edits of Rafelski Code/co2_decon_code_JLD'));
+addpath(genpath('/Users/juliadohner/Documents/MATLAB/Dohner_Rafelski_Code/co2_ocean_JLD'));
 
 %co2_model_code_JLD
-addpath(genpath('/Users/juliadohner/Documents/MATLAB/Dohner edits of Rafelski Code/co2_model_code_JLD'));
+addpath(genpath('/Users/juliadohner/Documents/MATLAB/Dohner_Rafelski_Code/co2_land_JLD'));
 
 %co2_model_data
-addpath(genpath('/Users/juliadohner/Documents/MATLAB/Dohner edits of Rafelski Code/co2_model_data'));
+addpath(genpath('/Users/juliadohner/Documents/MATLAB/Dohner_Rafelski_Code/co2_data'));
 
 
 
