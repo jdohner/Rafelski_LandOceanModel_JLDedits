3c936c71a9f0b680caae646839fb4b61b1e5f09c
Julia Dohner <juliadohner@jdohner-imac.ucsd.edu>
2017-10-17 15:16:06 -0700

    adding files to forward model

---
+++ co2_forward/BioBoxResponse.m
@@ -0,0 +1,70 @@
+% file BioBoxResponse.m
+% 
+% author Julia Dohner, borrowing from Lauren Rafelski
+% 
+% note Be sure to run defaults.m in outer folder before running this code
+% 
+% brief Calculates co2 uptake of fast land box model (driven by co2
+% fertilization)
+% 
+% CO2 fertilization only
+% code from bioboxtwo_sub10_annotate
+
+
+function [C1dt,C2dt,delCdt,delC1,delC2] = BioBoxResponse(eps,Q1a,Q2a,ts,year,dpCO2a,T)
+
+T0 = T(1,2);
+
+dt = 1/ts;
+
+% Define box sizes in ppm
+
+Catm = 600/2.12; % around 283 ppm (preindustrial)
+
+C1 = 110/2.12; % fast biosphere box, from old model
+C2 = 1477/2.12; % slow biosphere box, changed to be same as 1 box
+
+
+% Rate constants
+ K1a = 1/2.5; 
+ Ka1 = K1a*C1/Catm;
+
+K2a = 1/60; % slow box to atmosphere
+Ka2 = K2a*C2/Catm;
+
+% set up arrays
+
+delC1(:,1) = year(:,1);
+delC1(:,2) = zeros(size(year));
+delC2(:,1) = year(:,1);
+delC2(:,2) = zeros(size(year));
+delCdt(:,1) = year(:,1);
+C1dt(:,1) = year(:,1);
+C2dt(:,1) = year(:,1);
+delC1(length(year)+1,1) = year(length(year),1)+dt;
+delC2(length(year)+1,1) = year(length(year),1)+dt;
+
+for i = 1:length(year)
+    
+    % fast box
+    
+    C1dt(i,2) = Ka1*(Catm + eps*dpCO2a(i,2)) - K1a*Q1a^((T(i,2)-T0)/10)*(C1 + delC1(i,2)); % temperature-dependent respiration
+        
+   % C1dt(i,2) = Ka1*(Catm + eps*dpCO2a(i,2))*(1 + Q1a*(T(i,2)-T0)) - K1a*(C1 + delC1(i,2)); % temperature-dependent photosynthesis
+    
+    % slow box
+    
+    C2dt(i,2) = Ka2*(Catm + eps*dpCO2a(i,2)) - K2a*Q2a^((T(i,2)-T0)/10)*(C2 + delC2(i,2)); % temperature-dependent respiration
+        
+   % C2dt(i,2) = Ka2*(Catm + eps*dpCO2a(i,2))*(1 + Q2a*(T(i,2)-T0)) - K2a*(C2 + delC2(i,2)); % temperature dependent photosynthesis  
+    
+    % box total change in concentrations
+    
+    delC1(i+1,2) = sum(C1dt(:,2))*dt;
+    delC2(i+1,2) = sum(C2dt(:,2))*dt;
+    
+    % total flux into land
+    
+    delCdt(i,2) = C2dt(i,2) + C1dt(i,2);
+    
+end
---
+++ co2_forward/HILDAResponse.m
@@ -0,0 +1,42 @@
+% file HILDAResponse.m
+% 
+% author Julia Dohner, borrowing from Lauren Rafelski
+% 
+% note Be sure to run defaults.m in outer folder before running this code
+% 
+% brief Calculates the response "r" from the HILDA model (A.2.2 in Joos
+% 1996)
+% 
+% code from jooshildascale_annotate2.m (aka ocean model driver)
+
+function [t,r]= HILDAResponse(year)
+
+% allocating space for t and r matrices before for loop
+
+% tracer concentration (Joos 1996 A.1. pg 415)
+% one column matrix
+t = NaN(length(year), 1); 
+
+% pulse response function (Joos 1996 A.1. pg 415)
+% two column matrix
+r = NaN(length(year), 2); 
+
+% Response function to calculate ocean uptake
+for i = 1:length(year)
+    % each value in first column of t is the year at index i minus first
+    % year
+     t(i,1) = year(i) - year(1);
+     r(i,1) = t(i,1);
+    %Calculate response function based on HILDA equation in Joos 1996
+     if t(i,1) == 0
+         r(i,2) = 1;
+     elseif t(i,1) <= 2
+         r(i,2)= (1/0.95873)*(0.12935+0.21898*exp(-t(i,1)/0.034569)+0.17003*exp(-t(i,1)/0.26936)...
+             +0.24071*exp(-t(i,1)/0.96083)+0.24093*exp(-t(i,1)/4.9792));
+     else
+         r(i,2) = (1/0.95873)*(0.022936+0.24278*exp(-t(i,1)/1.2679)+0.13963*exp(-t(i,1)/5.2528)...
+                +0.089318*exp(-t(i,1)/18.601)+0.037820*exp(-t(i,1)/68.736)...
+                +0.035549*exp(-t(i,1)/232.3));
+     end
+end
+end
